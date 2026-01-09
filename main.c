212119746b51627cc01ca0e0fa347260b94aec7d
Fredmaster08 <frxdmaster@gmail.com>
2026-01-09 16:38:20 +0100

    src

---
+++ main.c
@@ -0,0 +1,86 @@
+
+#include <stdio.h>
+#include "raylib.h"
+
+#define WHEAT_COUNT 6
+#define DEFAULT_COOLDOWN 5.0f
+
+typedef struct {
+    int stage;
+    float cooldown; // secs
+}Wheat;
+
+void updateWheats(Wheat wheat[], float dt) {
+    for(int i = 0; i < WHEAT_COUNT; i++) {
+        wheat[i].cooldown -= dt;
+        if(wheat[i].cooldown <= 0.0f) {
+            if(wheat[i].stage < 4) wheat[i].stage++; 
+            wheat[i].cooldown = DEFAULT_COOLDOWN;
+        }
+    }
+}
+
+void drawWheats(Wheat wheat[]) {
+    for(int i = 0; i < WHEAT_COUNT; i++) {
+        int stage = wheat[i].stage;
+        Color color;
+        if(stage == 4) color = YELLOW;
+        else if(stage == 3) color = GREEN;
+        else if(stage == 2) color = DARKGREEN;
+        else if(stage == 1) color = DARKBROWN;
+    
+        DrawRectangle(i * 100 + 100, 100, 50, 50, color);
+    }
+}
+
+int main() {
+
+    Wheat wheat[WHEAT_COUNT];
+    wheat[0].stage = 1;
+    wheat[1].stage = 2;
+    wheat[2].stage = 3;
+    wheat[3].stage = 4;
+
+    wheat[0].cooldown = DEFAULT_COOLDOWN;
+    wheat[1].cooldown = DEFAULT_COOLDOWN;
+    wheat[2].cooldown = DEFAULT_COOLDOWN;
+    wheat[3].cooldown = DEFAULT_COOLDOWN;
+
+    int score = 0;
+
+    InitWindow(800, 600, "Test");
+    SetTargetFPS(60);
+
+    while(!WindowShouldClose()) {
+        float dt = GetFrameTime();
+
+        if(IsMouseButtonPressed(MOUSE_BUTTON_LEFT)) {
+            Vector2 mousePos = GetMousePosition();
+            for(int i = 0; i < WHEAT_COUNT; i++) {
+                float posX = i * 100 + 100;
+                float posY = 100;
+                float width = 50;
+                float height = 50;
+                if(posX < mousePos.x && posY < mousePos.y && mousePos.x < posX + width && posY < posY + height) {                  
+                    if(wheat[i].stage > 1) {
+                        score += wheat[i].stage; 
+                        wheat[i].stage = 1;
+                        wheat[i].cooldown = DEFAULT_COOLDOWN; 
+                    }
+                }
+            }
+        }
+        updateWheats(wheat, dt);
+
+        BeginDrawing();
+
+        ClearBackground(BLACK);
+        drawWheats(wheat);
+        DrawText(TextFormat("Score: %d", score), 10, 10, 20, RAYWHITE);
+
+        EndDrawing();
+    }
+    CloseWindow();
+
+    return 0;
+}
