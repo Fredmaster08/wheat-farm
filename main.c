690f7e56ac3bb3b3770d31e6d8eefd80c940e6ea
Fredmaster08 <frxdmaster@gmail.com>
2026-01-12 14:11:30 +0100

    expire date added

--- main.c
+++ main.c
@@ -6,22 +6,26 @@
 
 #define WHEAT_COUNT 20
 #define DEFAULT_COOLDOWN 5.0f
+#define DEFAULT_DURABILITY 20.0f
 
 typedef struct {
     int stage;
     float cooldown; // secs
+    float durability;
 }Wheat;
 
-void initalWheats(Wheat wheat[]) {
+void initWheats(Wheat wheat[]) {
     for(int i = 0; i < WHEAT_COUNT; i++) {
         wheat[i].stage = rand() % 3 + 1;
         wheat[i].cooldown = DEFAULT_COOLDOWN;
+        wheat[i].durability = DEFAULT_DURABILITY;
     }
 }
 
 void updateWheats(Wheat wheat[], float dt) {
     for(int i = 0; i < WHEAT_COUNT; i++) {
         wheat[i].cooldown -= dt;
+        wheat[i].durability -= dt;
         if(wheat[i].cooldown <= 0.0f) {
             if(wheat[i].stage < 4) wheat[i].stage++; 
             wheat[i].cooldown = DEFAULT_COOLDOWN;
@@ -37,7 +41,7 @@ void drawWheats(Wheat wheat[]) {
         else if(stage == 3) color = GREEN;
         else if(stage == 2) color = DARKGREEN;
         else if(stage == 1) color = DARKBROWN;
-    
+        if(wheat[i].durability <= 0) color = WHITE;
         DrawRectangle((i % 4) * 100 + 100, (i / 4)  * 100 + 100, 50, 50, color);
     }
 }
@@ -45,7 +49,7 @@ void drawWheats(Wheat wheat[]) {
 int main() {
 
     Wheat wheat[WHEAT_COUNT];
-    initalWheats(wheat);
+    initWheats(wheat);
 
     int score = 0;
 
@@ -62,11 +66,12 @@ int main() {
                 float posY = (i / 4) * 100 + 100;
                 float width = 50;
                 float height = 50;
-                if(posX < mousePos.x && posY < mousePos.y && mousePos.x < posX + width && mousePos.y < posY + height) {                
+                if(posX < mousePos.x && posY < mousePos.y && mousePos.x < posX + width && mousePos.y < posY + height) {              
                     if(wheat[i].stage > 1) {
                         score += wheat[i].stage; 
                         wheat[i].stage = 1;
-                        wheat[i].cooldown = DEFAULT_COOLDOWN; 
+                        wheat[i].cooldown = DEFAULT_COOLDOWN;
+                        wheat[i].durability = DEFAULT_DURABILITY;
                     }
                 }
             }
